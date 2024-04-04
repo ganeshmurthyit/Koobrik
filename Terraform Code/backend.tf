d089b7abb0391ebcd6318c54ddfff5c1f8cde3e2
ganeshmurthyit <157602576+ganeshmurthyit@users.noreply.github.com>
2024-04-04 11:18:26 +0530

    Update backend.tf

--- Terraform Code/backend.tf
+++ Terraform Code/backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket  = "koobrik-testing"                     // s3 bucket name
     key     = "tfstate/ucgrants.tfstate" //folder path in bucket to store state file
     region  = "us-east-2"                      // bucket region
-    encrypt = false
+    encrypt = true
     # dynamodb_table = "ktkterraformnew-lock"
 
   }
